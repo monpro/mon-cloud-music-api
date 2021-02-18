c7238168ad806dcf96a74e1e1bcb37f68d49e22f
monpro <monproshen@gmail.com>
2021-02-18 19:30:28 +1100

    fix the error of controller tests not running properly

--- src/main/java/com/monpro/cloudmusic/controller/home/HomeController.java
+++ src/main/java/com/monpro/cloudmusic/controller/home/HomeController.java
@@ -1,15 +1,21 @@
 package com.monpro.cloudmusic.controller.home;
 
-import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HomeControllerTest {
+public class HomeController {
 
   @RequestMapping(value = "/home", method = RequestMethod.GET)
   public String index() {
+
+    // get recommendation
+
+    // get today's list
+
+    // get ads
+
     return "cloud api setup";
   }
 }
